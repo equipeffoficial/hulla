e218f890d59319b18f83461ff90321a4d0c55fa6
Felipe Marques <equipeffoficial@gmail.com>
2021-01-08 13:50:56 -0400

    Foi efetuada a criação da classe de usuários.

---
+++ lib/screens/models/user.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class User{
+
+  User({this.name, this.email, this.password, this.id});
+
+  User.fromDocument(DocumentSnapshot document){
+    id = document.documentID;
+    name = document.data['name'] as String;
+    email = document.data['email'] as String;
+
+    }
+
+
+
+  String id;
+  String name;
+  String email;
+
+  String password;
+
+  String confirmPassword;
+
+}
